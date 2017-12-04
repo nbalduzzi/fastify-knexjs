4cf0484eb94d7a5bcb19de9366078d908d2d9cfa
Nicolas Balduzzi <nbalduzzi@zemsania.com>
2017-12-04 08:15:07 +0100

    compatible node v4

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const fastifyPlugin = require('fastify-plugin')
 const knex = require('knex')
 
-const fastifyKnexJS = (fastify, opts, next) => {
+function fastifyKnexJS (fastify, opts, next) {
   try {
     const handler = knex(opts)
     fastify.decorate('knex', handler)
