38b7c3ca85af3fc135289296705592708f5500fd
Valentin Radu <radu.v.valentin@me.com>
2021-03-31 00:24:46 +0300

    Added hook for knex instance destruction when the server exits

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ function fastifyKnexJS(fastify, opts, next) {
   try {
     const handler = knex(opts);
     fastify.decorate('knex', handler);
+
+    fastify.addHook('onClose', () => {
+      fastify.knex.destroy();
+    });
+
     next();
   } catch (err) {
     next(err);
